1e798d5d804ce06a3d7abca9b1a8ac26d3a205d9
lethihang111001 <lethihang111001@gmail.com>
2025-09-24 14:45:55 +0700

    Add upload test script 5

--- upload_test.py
+++ upload_test.py
@@ -1,15 +1,16 @@
 from clearml import Task
 
-# Tạo task đầu tiên
+# Tạo task
 task1 = Task.init(project_name="Examples", task_name="create_artifact")
 
-# Tạo file test
+# Tạo file local test
 local_file = "sample.json"
 with open(local_file, "w") as f:
     f.write('{"message": "Hello ClearML"}')
 
 # Upload file như artifact
 task1.upload_artifact(name="data file", artifact_object=local_file)
+print("Uploaded artifact:", local_file)
 
-# Kết thúc task1 để có thể khởi tạo task khác
+# Kết thúc task
 task1.close()
